15d2ad15d275ca87df6a8e0d46bf67e9423714c3
Jessie Hong <jessiejuheehong@berkeley.edu>
2022-07-05 22:57:58 +0900

    added camera view

--- main.js
+++ main.js
@@ -10,7 +10,14 @@ animate();
 function animate() {
     car.update();
     canvas.height = window.innerHeight;
+
+    context.save();
+    context.translate(0, -car.y + canvas.height * 0.5);
+
     road.draw(context);
     car.draw(context);
+
+    context.restore();
+
     requestAnimationFrame(animate);
 }
--- road.js
+++ road.js
@@ -28,17 +28,23 @@ class Road {
         context.lineWidth = 5;
         context.strokeStyle = "white";
 
-        for (let i=0; i<=this.laneCount; i++) {
+        for (let i=1; i<=this.laneCount-1; i++) {
             const x = lerp(this.left, this.right, i/this.laneCount);
-            if (i>0 && i<this.laneCount) {
-                context.setLineDash([20, 20]);
-            } else {
-                context.setLineDash([]);
-            }
+
+            context.setLineDash([20, 20]);
+            
             context.beginPath();
             context.moveTo(x, this.top);
             context.lineTo(x, this.bottom);
             context.stroke();
         }
+
+        context.setLineDash([]);
+        this.borders.forEach(border => {
+            context.beginPath();
+            context.moveTo(border[0].x, border[0].y);
+            context.lineTo(border[1].x, border[1].y);
+            context.stroke();
+        })
     }
 }
